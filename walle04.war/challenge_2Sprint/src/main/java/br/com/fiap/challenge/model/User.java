85cb74c1559ffbb4118fafb5cadfa797fabf196e
GabrielMontovani <montovani.devops@gmail.com.br>
2021-10-12 12:01:43 -0300

    walle04.war

---
+++ walle04.war/challenge_2Sprint/src/main/java/br/com/fiap/challenge/model/User.java
@@ -0,0 +1,75 @@
+package br.com.fiap.challenge.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User implements UserDetails{
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "{user.name.blank}")
+	private String name;
+	
+	@NotBlank(message = "{user.email.blank}")
+	@Email(message = "{user.email.invalid}")
+	private String email;
+	
+	@Size(min = 8, message = "{user.password.size}")
+	private String password;
+	
+	@NotBlank(message = "{user.github.blank}")
+	private String githubuser;
+	
+	public String getAvatarUrl() {
+		return "https://avatar.githubusercontent.com/" + this.githubuser;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	
+
+}
